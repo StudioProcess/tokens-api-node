392c4a53ee8fcdff2f9e2072c62f1f5eee7be723
Martin Grödl <martin@process.studio>
2021-04-12 11:02:09 +0200

    restructure for server (express.js) code

---
+++ db.mjs
@@ -0,0 +1,316 @@
+import { readFileSync } from 'fs';
+import got from 'got';
+
+export const DB = JSON.parse(readFileSync('./db.config.json'));
+
+/* 
+  databases:
+  tokens { _id, generated, keywords, svg, original_png }
+  interactions { _id, color, status, queue_position, token_id }
+  
+  interaction status progression:
+  incomplete -> new -> waiting -> done
+*/
+
+export async function request(method='get', path='', options={}) {
+  options = Object.assign({
+    method,
+    username: DB.user,
+    password: DB.pass,
+    responseType: 'json'
+  }, options);
+  return got(DB.url + path, options);
+}
+
+export async function create_db(name) {
+  const res = request('put', `/${name}`);
+  return res.body;
+}
+
+export async function delete_db(name) {
+  const res = request('delete', `/${name}`);
+  return res.body;
+}
+
+export async function create_filters(db_name) {
+  const res = request('post', `/${DB.interactions_db}`, {
+    json: {
+      _id: "_design/filters",
+      filters: {
+        "new": "function(doc, req) { return doc.status == 'new'; }",
+        "updates": "function(doc, req) { return doc._id == req.query.doc_id && (doc.status == 'waiting' || doc.status == 'done'); }"
+      }
+    }
+  });
+  return res.body;
+}
+
+export async function get_uuids(n=1) {
+  const res = await request('get', '/_uuids', {
+    searchParams: { 'count': n }
+  });
+  return res.body;
+}
+
+
+// Returns: { id: '' }
+export async function put_token(token) {
+  const res = await request('post', `/${DB.tokens_db}`, {json: token});
+  // res.body: { ok: true, id: '', rev: '' }
+  return { id: res.body.id };
+}
+
+// Returns: ''
+export async function delete_token(id) {
+  const res = await request('get', `/${DB.tokens_db}/${id}`);
+  const res1 = await request('delete', `/${DB.tokens_db}/${id}`, {
+    searchParams: { rev: res.body._rev }
+  });
+  // res1.body: { ok: true }
+  return '';
+}
+
+// Returns: { id, generated, svg }
+export async function get_single_token(id) {
+  const res = await request('get', `/${DB.tokens_db}/${id}`);
+  // res.body: { _id: '', _rev: '', token data ... }
+  return {
+    id: res.body._id,
+    svg: res.body.svg,
+    generated: res.body.generated
+  };
+}
+
+async function get_tokens_offset(offset=0, count=2, newest_first=true) {
+  if (offset < 0) offset = -1;
+  
+  const searchParams = {
+    'include_docs': true,
+    'skip': offset > 0 ? offset-1 : 0,
+    'limit': offset > 0 ? count + 2 : count + 1,
+    'descending': offset == -1,
+  };
+  if (newest_first) {
+    searchParams.descending = !searchParams.descending;
+  }
+  
+  const res = await request('get', `/${DB.tokens_db}/_all_docs`, {searchParams});
+  
+  const body = res.body;
+  body.rows = body.rows.map(row => {
+    row.doc.id = row.doc._id;
+    delete row.doc._id;
+    delete row.doc._rev;
+    return row.doc;
+  });
+  body.offset = offset;
+  body.newest_first = newest_first;
+  
+  if (offset < 0) {
+    body.rows.reverse();
+    body.offset = body.total_rows - body.rows.length + 1;
+  }
+  
+  if (offset > 0 || offset == -1) {
+    const first = body.rows.shift();
+    body.prev = first.id;
+  } else {
+    body.prev = null;
+  }
+  
+  if (body.rows.length > count) {
+    const last = body.rows.pop()
+    body.next = last.id;
+  } else {
+    body.next = null;
+  }
+  
+  return body;
+  
+  // TODO: throw error when offset >= total_rows
+}
+
+async function get_tokens_from_id(start_id, count=2, newest_first=true) {
+  const searchParams = {
+    'include_docs': true,
+    'limit': count + 1,
+    'descending': newest_first,
+    'start_key': `"${start_id}"`
+  };
+  
+  const res = await request('get', `/${DB.tokens_db}/_all_docs`, {searchParams});
+  
+  const body = res.body;
+  body.rows = body.rows.map(row => {
+    row.doc.id = row.doc._id;
+    delete row.doc._id;
+    delete row.doc._rev;
+    return row.doc;
+  });
+  body.newest_first = newest_first;
+  
+  if (body.rows.length > count) {
+    const last = body.rows.pop()
+    body.next = last.id;
+  } else {
+    body.next = null;
+  }
+  
+  // get previous (needs new request)
+  if (body.offset > 0) {
+    searchParams.limit = 2;
+    searchParams.descending = !searchParams.descending;
+    const res_prev = await request('get', `/${DB.tokens_db}/_all_docs`, {searchParams});
+    res_prev.body.rows = res_prev.body.rows.map(row => row.doc);
+    body.prev = res_prev.body.rows[1]._id;
+  } else {
+    body.prev = null;
+  }
+  
+  return body;
+}
+
+async function get_tokens_until_id(end_id, count=2, newest_first=true) {
+  const searchParams = {
+    'include_docs': true,
+    'limit': count + 1,
+    'descending': !newest_first,
+    'start_key': `"${end_id}"`
+  };
+  
+  const res = await request('get', `/${DB.tokens_db}/_all_docs`, {searchParams});
+  
+  const body = res.body;
+  body.rows = body.rows.map(row => {
+    row.doc.id = row.doc._id;
+    delete row.doc._id;
+    delete row.doc._rev;
+    return row.doc;
+  });
+  body.newest_first = newest_first;
+  body.rows.reverse();
+  
+  if (body.rows.length > count) {
+    const first = body.rows.shift();
+    body.prev = first.id;
+  } else {
+    body.prev = null;
+  }
+  
+  // get next (needs new request)
+  if (body.offset > 0) {
+    searchParams.limit = 2;
+    searchParams.descending = !searchParams.descending;
+    const res_next = await request('get', `/${DB.tokens_db}/_all_docs`, {searchParams});
+    res_next.body.rows = res_next.body.rows.map(row => row.doc);
+    body.next = res_next.body.rows[1]._id;
+  } else {
+    body.next = null;
+  }
+  
+  body.offset = body.total_rows - body.offset - body.rows.length;
+  return body;
+}
+
+// Returns: { total_rows, offset, rows: [ {id, ..}, ..], newest_first, prev, next }
+export async function get_tokens(offset=0, start_id=null, end_id=null, count=2, newest_first=true) {
+  if (offset != null) {
+    return get_tokens_offset(offset, count, newest_first);
+  }
+  
+  if (start_id != null) {
+    return get_tokens_from_id(start_id, count, newest_first);
+  }
+  
+  if (end_id != null) {
+    return get_tokens_until_id(end_id, count, newest_first);
+  }
+  
+  // TODO: error now
+}
+
+
+
+let color = -1;
+
+// Returns: { id, color }
+export async function request_interaction() {
+  color = (color + 1) % 10;
+  const next_color = color;
+  const res = await request('post', `/${DB.interactions_db}`, {
+    json: { 
+      'status': 'incomplete',
+      'color': next_color,
+    }
+  });
+  res.body.color = next_color;
+  return {
+    id: res.body.id,
+    color: res.body.color,
+  };
+}
+
+// Returns: ''
+export async function deposit_interaction(id, keywords) {
+  let res = await request('get', `/${DB.interactions_db}/${id}`);
+  let int = res.body;
+  int.status = 'new';
+  int.keywords = keywords;
+  res = await request('post', `/${DB.interactions_db}`, {json: int});
+  return '';
+}
+
+// Returns: { id, seq, queue_position, token_id? }
+export async function get_single_interaction_updates(id, since=0) {
+  const res = await request('get', `/${DB.interactions_db}/_changes`, {
+    searchParams: {
+      feed: 'longpoll',
+      filter: 'filters/updates',
+      doc_id: id,
+      include_docs: true,
+      since,
+    }
+  });
+  const result = res.body.results[0];
+  const doc = result.doc;
+  return {
+    id: doc._id,
+    seq: result.seq,
+    queue_position: doc.queue_position,
+    token_id: doc.token_id ?? null,
+  };
+}
+
+// Returns: ''
+export async function update_interaction(id, queue_position, token_id=null) {
+  let res = await request('get', `/${DB.interactions_db}/${id}`);
+  let int = res.body;
+  if (token_id != null) {
+    int.queue_position = 0;
+    int.token_id = token_id;
+    int.status = 'done';
+  } else {
+    int.queue_position = queue_position;
+    int.status = 'waiting';
+  }
+  res = await request('post', `/${DB.interactions_db}`, {json: int});
+  return '';
+}
+
+// Returns: { id, color, keywords }
+export async function get_new_interaction_updates(since=0) {
+  const res = await request('get', `/${DB.interactions_db}/_changes`, {
+    searchParams: {
+      feed: 'longpoll',
+      filter: 'filters/new',
+      include_docs: true,
+      since,
+    }
+  });
+  const doc = res.body.results[0].doc;
+  return {
+    id: doc._id,
+    color: doc.color,
+    keywords: doc.keywords
+  };
+}
--- db.test.mjs
+++ db.test.mjs
@@ -1,7 +1,6 @@
-import crypto from 'crypto';
 import tap from 'tap';
-import * as main from './main.mjs';
-import * as util from './utils.mjs';
+import * as db from './db.mjs';
+import * as util from './util.mjs';
 
 
 const tokens = [];
@@ -11,18 +10,18 @@ const match_id = /[0-9a-f]{32}/; // 32 hex digits
 tap.before(async () => {
   console.log('running before:');
   const rnd_id = util.rnd_hash(7);
-  main.DB.tokens_db += '-' + rnd_id;
-  main.DB.interactions_db += '-' + rnd_id;
+  db.DB.tokens_db += '-' + rnd_id;
+  db.DB.interactions_db += '-' + rnd_id;
   
-  console.log('creating db ' + main.DB.tokens_db);
-  await main.create_db(main.DB.tokens_db);
+  console.log('creating db ' + db.DB.tokens_db);
+  await db.create_db(db.DB.tokens_db);
   
-  console.log('creating db ' + main.DB.interactions_db);
-  await main.create_db(main.DB.interactions_db);
+  console.log('creating db ' + db.DB.interactions_db);
+  await db.create_db(db.DB.interactions_db);
   await util.sleep(500); // will fail if putting immediately
   
   console.log('creating filters');
-  await main.create_filters();
+  await db.create_filters();
   
   // create a bunch of token docs
   console.log('creating tokens');
@@ -31,7 +30,7 @@ tap.before(async () => {
       svg: util.random_svg(),
       generated: util.timestamp(),
     };
-    const res = await main.put_token(token);
+    const res = await db.put_token(token);
     token.id = res.id;
     tokens.push(token);
   }
@@ -40,10 +39,10 @@ tap.before(async () => {
 
 tap.teardown(async () => {
   console.log('running teardown:');
-  console.log('deleting db ' + main.DB.tokens_db);
-  await main.delete_db(main.DB.tokens_db);
-  console.log('deleting db ' + main.DB.interactions_db);
-  await main.delete_db(main.DB.interactions_db);
+  console.log('deleting db ' + db.DB.tokens_db);
+  await db.delete_db(db.DB.tokens_db);
+  console.log('deleting db ' + db.DB.interactions_db);
+  await db.delete_db(db.DB.interactions_db);
 });
 
 
@@ -54,20 +53,20 @@ tap.test('put token', async t => {
     svg: util.random_svg(),
     generated: util.timestamp(),
   };
-  const res = await main.put_token(token);
+  const res = await db.put_token(token);
   t.match(res, { id: match_id }, 'result is valid');
   
-  const res1 = await main.get_single_token(res.id);
+  const res1 = await db.get_single_token(res.id);
   t.same(res1, { id: res.id, svg: token.svg, generated: token.generated }, 'get successful');
   
-  const res2 =  await main.delete_token(res.id);
+  const res2 =  await db.delete_token(res.id);
   t.equal(res2, '', 'deleted sucessfully');
 });
 
 
 tap.test('get tokens by offset', async t => {
   // first page
-  let res = await main.get_tokens(0, null, null, 3, true);
+  let res = await db.get_tokens(0, null, null, 3, true);
   // console.log(res);
   t.same(res.rows[0], tokens[0]);
   t.same(res.rows[1], tokens[1]);
@@ -81,7 +80,7 @@ tap.test('get tokens by offset', async t => {
   });
   
   // last page
-  res = await main.get_tokens(-1, null, null, 3, true);
+  res = await db.get_tokens(-1, null, null, 3, true);
   // console.log(res);
   t.same(res.rows[0], tokens[tokens.length-3]);
   t.same(res.rows[1], tokens[tokens.length-2]);
@@ -95,7 +94,7 @@ tap.test('get tokens by offset', async t => {
   });
   
   // middle page
-  res = await main.get_tokens(4, null, null, 3, true);
+  res = await db.get_tokens(4, null, null, 3, true);
   // console.log(res);
   t.same(res.rows[0], tokens[4]);
   t.same(res.rows[1], tokens[5]);
@@ -111,7 +110,7 @@ tap.test('get tokens by offset', async t => {
 
 
 tap.test('get tokens from start id', async t => {
-  let res = await main.get_tokens(null, tokens[0].id, null, 3, true);
+  let res = await db.get_tokens(null, tokens[0].id, null, 3, true);
   // console.log(res);
   t.same(res.rows[0], tokens[0]);
   t.same(res.rows[1], tokens[1]);
@@ -124,7 +123,7 @@ tap.test('get tokens from start id', async t => {
     next: tokens[3].id,
   });
   // next page
-  res = await main.get_tokens(null, res.next, null, 3, true);
+  res = await db.get_tokens(null, res.next, null, 3, true);
   // console.log(res);
   t.same(res.rows[0], tokens[3]);
   t.same(res.rows[1], tokens[4]);
@@ -141,7 +140,7 @@ tap.test('get tokens from start id', async t => {
 
 tap.test('get tokens until end id', async t => {
   // last page
-  let res = await main.get_tokens(null, null, tokens[9].id, 3, true);
+  let res = await db.get_tokens(null, null, tokens[9].id, 3, true);
   // console.log(res);
   t.same(res.rows[0], tokens[7]);
   t.same(res.rows[1], tokens[8]);
@@ -154,7 +153,7 @@ tap.test('get tokens until end id', async t => {
     next: null,
   });
   // page before
-  res = await main.get_tokens(null, null, res.prev, 3, true);
+  res = await db.get_tokens(null, null, res.prev, 3, true);
   // console.log(res);
   t.same(res.rows[0], tokens[4]);
   t.same(res.rows[1], tokens[5]);
@@ -176,36 +175,36 @@ tap.test('interaction process', async t => {
   let token_id = '';
   
   t.test(async t => {
-    const res = await main.get_new_interaction_updates();
+    const res = await db.get_new_interaction_updates();
     t.equal(res.id, id, 'received new interaction update');
     t.same(res.keywords, keywords, 'with correct keywords');
     t.same(res.color, color, 'and correct color');
     
     await util.sleep(100);
-    const res2 = await main.update_interaction(res.id, 2);
+    const res2 = await db.update_interaction(res.id, 2);
     t.equal(res2, '', 'successful queue update (2)');
     await util.sleep(100);
-    const res3 = await main.update_interaction(res.id, 1);
+    const res3 = await db.update_interaction(res.id, 1);
     t.equal(res3, '', 'successful queue update (1)');
     await util.sleep(100);
     token_id = util.rnd_hash(32);
-    const res4 = await main.update_interaction(res.id, null, token_id);
+    const res4 = await db.update_interaction(res.id, null, token_id);
     t.equal(res4, '', 'successful token update');
   });
   
-  const res = await main.request_interaction();
+  const res = await db.request_interaction();
   id = res.id;
   color = res.color;
   t.match(res, {id: match_id, color:0 }, 'got interaction id and color');
   
-  const res1 = await main.deposit_interaction(res.id, keywords);
+  const res1 = await db.deposit_interaction(res.id, keywords);
   t.equal(res1, '', 'sucessfully deposited interaction');
   
-  const res2 = await main.get_single_interaction_updates(id);
+  const res2 = await db.get_single_interaction_updates(id);
   t.has(res2, {id, queue_position:2, token_id:null}, 'received queue update (2)');
-  const res3 = await main.get_single_interaction_updates(res2.id, res2.seq);
+  const res3 = await db.get_single_interaction_updates(res2.id, res2.seq);
   t.has(res3, {id, queue_position:1, token_id:null}, 'received queue update (1)');
-  const res4 = await main.get_single_interaction_updates(res3.id, res3.seq);
+  const res4 = await db.get_single_interaction_updates(res3.id, res3.seq);
   t.has(res4, {id, queue_position:0, token_id}, 'received token update');
 });
 
