2353c8a7a610ef2495ae70827daa3215664ec1c0
Martin Grödl <martin@process.studio>
2021-10-13 11:31:13 +0200

    updated qr code scripts

--- make_qr_2021-10.mjs
+++ make_qr_2021-10.mjs
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 /* 
   quick script to generate exihition qr codes
+  for exhibition in mak forum, 10/2021
 */
 import { make as make_jwt, save_qr } from './make_jwt.mjs';
 
 const BASE_URL = 'https://tokensforclimate.care/generate/?auth=';
-const FROM = '2021-05-17';
-const DAYS = 45;
-const TIMEZONE = "GMT+1";
+const FROM = '2021-10-13';
+const DAYS = 40;
+const TIMEZONE = "GMT+2";
 
 let date = new Date(FROM);
 
