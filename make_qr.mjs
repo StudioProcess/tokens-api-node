3460caeebcb286b941e0ad1f3d5c5b4b4931b8fc
Martin Grödl <martin@process.studio>
2023-02-28 11:17:55 +0000

    QR code for HSLU talk

--- make_qr.mjs
+++ make_qr.mjs
@@ -19,6 +19,9 @@
   used 2022-11:
     # for supsi mendrisio
     ./make_qr.mjs --single '2022-12-13 00:00' '2022-12-20 00:00'
+  used 2023-02:
+    # for HSLU hochschule luzern (online talk)
+    ./make_qr.mjs --single '2023-02-28 00:00' '2023-03-13 00:00'
 */
 
 import url from 'url';
